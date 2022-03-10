b68e7b5c6f162c4c9fe7c476e8751a6a131259c9
yamauchi1105454 <akio.yamauchi@kusurinomadoguchi.co.jp>
2022-03-10 11:19:18 +0900

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,21 @@
+<?php
+
+class Config {
+
+
+    //トークンの作成
+    public static function createToken() {
+        $token_byte = openssl_random_pseudo_bytes(16);
+        $csrf_token = bin2hex($token_byte);
+        $_SESSION['csrf_token'] = $csrf_token;
+    }
+
+    //入力文字のエスケープ処理
+    //Config::h($変数) で使用可能
+    public static function h($str) {
+        return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+    }
+    
+
+
+}
