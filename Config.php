9f3c2459b44ce5a12a46501116cc457515cedccd
yamauchi1105454 <akio.yamauchi@kusurinomadoguchi.co.jp>
2022-03-10 11:47:30 +0900

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,21 +1,21 @@
 <?php
+############################################################
+#設定に関する処理をまとめたクラスです。
+############################################################
 
 class Config {
 
-
-    //トークンの作成
+    //トークンの作成　Config::createToken() で使用可能
     public static function createToken() {
         $token_byte = openssl_random_pseudo_bytes(16);
         $csrf_token = bin2hex($token_byte);
         $_SESSION['csrf_token'] = $csrf_token;
     }
 
-    //入力文字のエスケープ処理
-    //Config::h($変数) で使用可能
+    //入力文字のエスケープ処理　Config::h($変数) で使用可能
     public static function h($str) {
         return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
     }
     
-
 
 }
