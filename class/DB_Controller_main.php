db5dbe6574029d1e6f3dd159632e62ebb9508b1e
AkitoOkamoto <Akito.Okamoto@kusurinomadoguchi.co.jp>
2022-03-11 10:45:16 +0900

    update: mainテーブル操作用子クラス案A

--- class/DB_Controller_main.php
+++ class/DB_Controller_main.php
@@ -13,7 +13,8 @@ class DB_Controller_main extends DB_Controller {
     // （nullはそのままstringにバインドできないため）
     public function insert_a_record($title, $payment, $payment_at, $user_id, $type_id, $category_id, $group_id, $memo = '') {
         if($this->connect_DB()) {
-            $stmt = $this->pdo->prepare('INSERT INTO articles(title, memo, payment, payment_at, user_id, type_id, category_id, group_id) VALUES(:title, :memo, :payment, :payment_at, :user_id, :type_id, :category_id, :group_id);');
+            $sql = 'INSERT INTO articles(title, memo, payment, payment_at, user_id, type_id, category_id, group_id) VALUES(:title, :memo, :payment, :payment_at, :user_id, :type_id, :category_id, :group_id);';
+            $stmt = $this->pdo->prepare($sql);
             //SQL文中の プレース部を 定義しておいた変数に置き換える
             $stmt->bindParam( ':title',         $title,         PDO::PARAM_STR);
             $stmt->bindParam( ':memo',          $memo,          PDO::PARAM_STR);
@@ -69,15 +70,16 @@ class DB_Controller_main extends DB_Controller {
             return $results; //格納されていなければ false を返す
         }
     }
-    /***** filtering 方法について要検討 ******/
+
+    /***** 複数条件で行うselect文を1つのメソッドにまとめる案 ******/
     // カテゴリでfilterする条件式を生成する 
-    public function filter_by_a_category($category_id) {
+    public function filter_by_a_category($group_id, $category_id) {
         if($this->connect_DB()) {
-            $sql = 'SELECT * FROM `full_records` WHERE `group_id`=:group_id;';
-            echo $sql;
+            $sql = 'SELECT * FROM `full_records` WHERE `group_id`=:group_id AND `category_id`=:category_id;';
             $stmt = $this->pdo->prepare($sql);
             //SQL文中の プレース部を 定義しておいた変数に置き換える
-            $stmt->bindParam( ':group_id',   $category_id,   PDO::PARAM_INT);
+            $stmt->bindParam( ':group_id',      $group_id,      PDO::PARAM_INT);
+            $stmt->bindParam( ':category_id',   $category_id,   PDO::PARAM_INT);
 
             //sqlを 実行
             $stmt->execute();
