52930bf5279416d6e0cd9c3478a106b72b633fc7
AkitoOkamoto <Akito.Okamoto@kusurinomadoguchi.co.jp>
2022-03-15 13:42:28 +0900

    update: 引数と変数定義文に型宣言を追加

--- class/DB_Controller.php
+++ class/DB_Controller.php
@@ -8,11 +8,9 @@ class DB_Controller {
 
     protected String $target_table;
     
-    protected bool $is_ready;
-    
 
     // 対象テーブルを選択
-    function __construct($target_table) {
+    function __construct(string $target_table) {
         $this->target_table = $target_table;
         self::connect_DB(); //PDOオブジェクトを生成
         // 以下でPDOの設定を行う
@@ -23,6 +21,7 @@ class DB_Controller {
     * DBに接続するメソッド 接続成功時にtrue を返す。それぞれのメソッドの開始時に呼び出す
     * 切断（= PDO オブジェクトへの null の代入）は下記のそれぞれのメソッドの終わりに行う
     *****************************************************************************/
+    // DBとの接続処理を行う (基本的に内部で呼び出すため)
     public static function connect_DB() {
         if(!isset(self::$pdo)) {
             try{
@@ -36,12 +35,16 @@ class DB_Controller {
             }
         }
     }
+    // DBとの切断処理を行う
+    public static function disconnect_DB() {
+        self::$pdo = null;
+    }
 
     /**************************************************************************
      * 基本メソッド（idのみで行えるDB操作）
      **********************************************************************/
     // select * from 対象テーブル where = 指定したid
-    public function fetch_a_record($target_id) {
+    public function fetch_a_record(int $target_id) {
         if(isset(self::$pdo) || self::connect_DB()) {
             $sql = "SELECT *
                     FROM `{$this->target_table}`
@@ -62,7 +65,7 @@ class DB_Controller {
     }
 
     // select * from 対象テーブル
-    public function fetch_all_records($order = 0) {
+    public function fetch_all_records(int $order = 0) {
         if(isset(self::$pdo) || self::connect_DB()) {
             // 昇順・降順を選択する
             $order_clause = $this->select_order($order);
@@ -74,7 +77,6 @@ class DB_Controller {
             $stmt->execute();
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            
             return $results;
 
         } else {
@@ -83,7 +85,7 @@ class DB_Controller {
         }
     }
     // delete from 対象テーブル
-    public function delete_a_record($target_id) {
+    public function delete_a_record(int $target_id) {
         if(isset(self::$pdo) || self::connect_DB()) {
             $sql = "DELETE FROM
                     ` {$this->target_table} `
@@ -93,18 +95,13 @@ class DB_Controller {
             $stmt->bindParam( ':id', $target_id, PDO::PARAM_INT);
             $stmt->execute();
 
-            
         } else {
             // 接続失敗時はstringでエラーメッセージを返す
             return self::$connect_error;
         }
     }
-    // DBとの切断処理を行う
-    public static function disconnect_DB() {
-        self::$pdo = null;
-    }
     //order by句を返す
-    protected function select_order($order = 0, $culmun = 'id') {
+    protected function select_order(int $order = 0, string $culmun = 'id') {
         switch($order){
             case 1:
                 $order_clause = "order by `{$culmun}` asc";
