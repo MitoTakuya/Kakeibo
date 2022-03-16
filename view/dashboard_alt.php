db5056fe1dd73f39651fcd0438b67e4d2882aac6
AkitoOkamoto <Akito.Okamoto@kusurinomadoguchi.co.jp>
2022-03-16 14:16:12 +0900

    update: 実装部分の下書き

--- view/dashboard_alt.php
+++ view/dashboard_alt.php
@@ -14,11 +14,13 @@
     </head>
 
     <body>
+        <div id="dashboard">
+
         <header>
             <nav class="navbar navbar-dark bg-dark">
                 <a href="#" class="navbar-brand">家計簿(グループ名)</a> 
                 <div class="btn-group" role="group">
-                    <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button id="header_menu" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Menu
                     </button>
                     <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
@@ -28,31 +30,68 @@
                 </div>
             </nav>
         </header>
-    
+
+        <!-- 内容部分 -->
         <div class="container">
             <div class="mx-auto">
                 <div class="card card_property">
-                    <svg class="card-img-top" width="auto" height="50" xmlns="http://www.w3.org/2000/svg" focusable="false" role="img" >
+                    <svg class="card-img-top" width="320" height="50" xmlns="http://www.w3.org/2000/svg" focusable="false" role="img" >
                         <rect width="100%" height="100%" fill="#87CEEB"/>
                         <text class="mx-auto" x="50%" y="50%" fill="#ffffff" dy=".5em" text-anchor="middle">何か文字入れられる</text>
                     </svg>
                     <p>(ここに「〇月の支出」と表示する)</p>
+                        <div class="form-group"> <!-- 可能なら右に寄せたい -->
+                        <select class="w-25 form-control select_period">
+                            <option value="">--月を選択--</option>
+                            <!-- vue.jsで実装する -->
+                            <option value="hamster">vue.jsで実装する</option>
+                            <option value="dog">登録月</option>
+                            <option value="cat">次月</option>
+                            <option value="hamster">次々月</option>
+                        </select>
+                        </div>
                     <div class="outgo_chart card-body mx-auto">
                             <img src="https://www.illustkit.com/wp/wp-content/uploads/2021/05/IK-2021-0228-69.png" width="500" height="500">
                     </div>
                     <p>(これは仮置きのグラフ)</p>
                     </div>
             </div>
+
             <div class="mx-auto">
                 <div class="card card_property">
                     <p>ここにカテゴリーごとにまとめた支出のテーブルを作る</p>
                         <div class="outgo_chart card-body mx-auto">
                                 <img src="https://shop.woodworks-marutoku.com/kanri/wp-content/uploads/2019/09/190926_ta_top.jpg" width="720" height="400">
+                                <p>(これは仮置きの画像)</p>
+                                <p>(下記のような表を作成するテーブル)</p>
+                                <table class="table">
+                                    <!-- 後で colの属性を指定する -->
+                                    <thead>
+                                        <th>カテゴリー</th>
+                                        <th>支出額</th>
+                                        <th>(前月比とか？)</th>
+                                    </thead>
+                                    <tr>
+                                        <td>食費</td>
+                                        <td>50,000 円</td>
+                                        <td>tbl_test</td>
+                                    </tr>
+                                    <tr>
+                                        <td>交通費</td>
+                                        <td>tbl_test</td>
+                                        <td>tbl_test</td>
+                                    </tr>
+                                    <tr>
+                                        <td>娯楽費</td>
+                                        <td>tbl_test</td>
+                                        <td>tbl_test</td>
+                                    </tr>
+                                </table>
                         </div>
-                    <p>(これは仮置きの画像)</p>
                 </div>
             </div>
         </div> <!-- container の終わり -->
+        </div> <!-- #app の終わり-->
 
         <footer class="footer text-light bg-dark">
             <nav class="navbar navbar-dark bg-dark">
@@ -64,5 +103,48 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <!-- Vue.js下書き -->
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+        <script>
+            let app = new Vue({
+            el :'#dashboard',
+            data : {
+                v_content : '', // これがセットされ直しててしまう？ 入力しなおしても vueで内部的に「content + response.data」が保存されてしまっている？
+                placeholder : "content ...\n...\nURLs",
+                add_vid_btn : "btn btn-primary",
+                add_score_btn : "btn btn-primary",
+                video_id : null,
+                flat_score_url : null,
+                target_url_input : null,
+                target_video_url : null,
+                target_flat_score_url : null
+            },
+            methods : {
+                add_video : function(event) {
+                    //該当phpファイルに動画のurlを送り 動画IDを抽出させるメソッド。
+                    let target_video_url = this.target_video_url;
+                    let params = new URLSearchParams();
+                    params.append('target_video_url', this.target_video_url);
+                    axios
+                    .post('Data/analyse_video_url.php', params)
+                    .then(response => (
+                        this.video_id = response.data
+                    ));
+                    this.add_vid_btn = "btn btn-success";
+                    //console.log("-----end of function shorten_url-----------------------------");
+                },
+                reset_add_vid_btn : function (event) {
+                    if (this.target_video_url == "") {
+                        this.add_vid_btn = "btn btn-primary";
+                    }
+                },
+                reset_add_score_btn : function (event) {
+                    if (this.target_flat_score_url == "") {
+                        this.add_score_btn = "btn btn-primary";
+                    }
+                }
+            }
+        })
+        </script>
     </body>
 </html>
