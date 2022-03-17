5a46f07b841511c03a960c41949532f2f248b2df
MitoTakuya <Takuya.Mito@kusurinomadoguchi.co.jp>
2022-03-17 11:12:20 +0900

    Fix:PSR-2に基づき修正

--- class/DB_Connector_users.php
+++ class/DB_Connector_users.php
@@ -1,61 +1,66 @@
 <?php
-require_once dirname(__FILE__) . '/DB_Controller.php';
-class DB_Controller_users extends DB_Controller {
+require_once dirname(__FILE__) . '/DB_Connector.php';
+class DB_Connector_users extends DB_Connector
+{
     public static $user_errors = array();
     private $group_id = 0;
 
     // 対象テーブルを選択
-    function __construct() {
+    function __construct()
+    {
         parent::__construct('users');
     }
 
-    public function setGroupId($group_id) {
+    public function setGroupId($group_id)
+    {
         $this->group_id = $group_id;
     }
-    public function getGroupId() {
+    public function getGroupId()
+    {
         return $this->group_id;
     }
 
     // ユーザー登録入力内容チェック
-    public function input_confirmation() {
-        if(!isset($_POST['user_name']) || str_replace(array(" ", "　"), "", $_POST['user_name']) === '') {
+    public function inputConfirmation()
+    {
+        if (!isset($_POST['user_name']) || str_replace(array(" ", "　"), "", $_POST['user_name']) === '') {
             self::$user_errors['user_name'] = '名前を入力してください';
-        } else if(mb_strlen($_POST['user_name']) > 30) {
+        } elseif (mb_strlen($_POST['user_name']) > 30) {
             self::$user_errors['user_name'] = '名前は30文字以内で入力してください';
         } else {
             $user_name = $_POST['user_name'];
         }
 
-        if(trim($_POST['mail']) === "") {
+        if (trim($_POST['mail']) === "") {
             self::$user_errors['mail'] = "メールアドレスを入力してください";
         } else {
             $mail = $_POST['mail'];
-            $this->check_duplicate($mail);
+            $this->checkDuplicate($mail);
         }
 
-        if(trim($_POST['password']) === "") {
+        if (trim($_POST['password']) === "") {
             self::$user_errors['password'] = "パスワードを入力してください";
-        }else if (strlen($_POST['password']) < 4) {
+        } elseif (strlen($_POST['password']) < 4) {
             self::$user_errors['password'] = 'パスワードを4文字以上で入力してください';
         } else {
             $password = $_POST['password'];
         }
 
-        if(!empty($_FILES['user_image']['name'])) {
+        if (!empty($_FILES['user_image']['name'])) {
             $ext = substr($_FILES['user_image']['name'], -3);
-            if($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
+            if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
                 self::$user_errors['user_image'] = '画像の形式は[jpg],[gif],[png]のみです。';
             }
-        } else if (empty($_FILES['user_image']['name'])) {
+        } elseif (empty($_FILES['user_image']['name'])) {
             self::$user_errors['user_image'] = '画像を選択してください';
         }
 
         // ユーザーグループ
         // 新規グループ選択時
-        if($_POST['user_group'] == "new_group") {
-            if(!isset($_POST['group_form']) || str_replace(array(" ", "　"), "", $_POST['group_form']) === '') {
+        if ($_POST['user_group'] == "new_group") {
+            if (!isset($_POST['group_form']) || str_replace(array(" ", "　"), "", $_POST['group_form']) === '') {
                 self::$user_errors['group_form'] = '家計簿名を入力してください';
-            } else if(mb_strlen($_POST['group_form']) > 30) {
+            } elseif (mb_strlen($_POST['group_form']) > 30) {
                 self::$user_errors['group_form'] = '30文字以内で入力してください';
             } else {
                 $group_name = $_POST['group_form'];
@@ -63,16 +68,16 @@ class DB_Controller_users extends DB_Controller {
                 $group_password =  uniqid();
             }
             // 既存グループ選択時
-        } elseif($_POST['user_group'] == "existing_group") {
-            if(!isset($_POST['group_form']) || str_replace(array(" ", "　"), "", $_POST['group_form']) === '') {
+        } elseif ($_POST['user_group'] == "existing_group") {
+            if (!isset($_POST['group_form']) || str_replace(array(" ", "　"), "", $_POST['group_form']) === '') {
                 self::$user_errors['group_form'] = 'グループパスワードを入力してください';
-            } else if(mb_strlen($_POST['group_form']) > 30) {
+            } elseif (mb_strlen($_POST['group_form']) > 30) {
                 self::$user_errors['group_form'] = '30文字以内で入力してください';
             } else {
                 $group_password = $_POST['group_form'];
                 // パスワードからuser_groupのidを検索
-                $error = $this->search_group_id($group_password);
-                if(!is_array($error)) {
+                $error = $this->searchGroupId($group_password);
+                if (!is_array($error)) {
                     self::$user_errors['group_form'] = 'グループパスワードが違います。';
                 } else {
                     $this->setGroupId($error['id']);
@@ -88,13 +93,13 @@ class DB_Controller_users extends DB_Controller {
             // 画像をアップロード
             move_uploaded_file($_FILES['user_image']['tmp_name'], '../images/'. $user_image);
             
-            if($_POST['user_group'] ==  "new_group") {
+            if ($_POST['user_group'] ==  "new_group") {
                 // ユーザー、新規グループ登録
-                $this->create_user_with_group($group_name, $group_password, $user_name, $hash, $mail, $user_image);
+                $this->createUserWithGroup($group_name, $group_password, $user_name, $hash, $mail, $user_image);
             } else {
                 // ユーザー登録
                 $group_id = $this->getGroupId();
-                $this->insert_an_user($user_name, $hash, $mail, $user_image, $group_id);
+                $this->insertUser($user_name, $hash, $mail, $user_image, $group_id);
             }
             return "ok";
         } else {
@@ -103,30 +108,31 @@ class DB_Controller_users extends DB_Controller {
     }
     
     // ログイン入力確認
-    public function login_confirmation() {
+    public function loginConfirmation()
+    {
 
         // POSTされていないときは処理を中断する
-        if(!filter_input_array(INPUT_POST)) {
+        if (!filter_input_array(INPUT_POST)) {
             return;
         }
         
         // メールアドレスが入力されているか確認
-        if(trim($_POST['mail']) === "") {
+        if (trim($_POST['mail']) === "") {
             self::$user_errors['login_mail'] = "メールアドレスを入力してください";
         } else {
             $mail = $_POST['mail'];
         }
 
         // パスワードが入力されているか確認
-        if(trim($_POST['password']) === "") {
+        if (trim($_POST['password']) === "") {
             self::$user_errors['login_password'] = "パスワードを入力してください";
         } else {
             $password = $_POST['password'];
         }
 
         // エラーがなければ保存
-        if(count(self::$user_errors) == 0) {
-            $this->login_user($mail, $password);
+        if (count(self::$user_errors) == 0) {
+            $this->loginUser($mail, $password);
             return "login";
         } else {
             return self::$user_errors;
@@ -139,14 +145,14 @@ class DB_Controller_users extends DB_Controller {
     }
 
     // ユーザー・新規グループ登録
-    public function create_user_with_group($group_name, $group_password, $user_name, $hash, $mail, $user_image)
+    public function createUserWithGroup($group_name, $group_password, $user_name, $hash, $mail, $user_image)
     {
         try {
             self::$pdo->beginTransaction();
 
-            $this->insert_user_group($group_name, $group_password);
+            $this->insertUserGroup($group_name, $group_password);
             $group_id = $this->getGroupId();
-            $this->insert_an_user($user_name, $hash, $mail, $user_image, $group_id);
+            $this->insertUser($user_name, $hash, $mail, $user_image, $group_id);
 
             self::$pdo->commit();
         } catch (PDOException $e) {
@@ -156,15 +162,16 @@ class DB_Controller_users extends DB_Controller {
     }
 
     // ログイン用メソッド
-    public function login_user($mail, $password) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+    public function loginUser($mail, $password)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('SELECT `id`, `password`, `mail` FROM users WHERE mail=:mail');
             //SQL文中の プレース部を 定義しておいた変数に置き換える
             $stmt->bindParam( ':mail', $mail, PDO::PARAM_STR);
             $stmt->execute();
             $user = $stmt->fetch();
             // 指定したハッシュがパスワードにマッチしているか
-            if(password_verify($password, $user['password'])) {
+            if (password_verify($password, $user['password'])) {
                 // ユーザー情報をセッションに保存
                 $_SESSION['id'] = $user['id'];
                 return "login_ok";
@@ -177,15 +184,16 @@ class DB_Controller_users extends DB_Controller {
     }
 
     // メールアドレス重複確認
-    public function check_duplicate($mail) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+    public function checkDuplicate($mail)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('SELECT COUNT(mail) as cnt FROM users WHERE mail=:mail');
             $stmt->bindParam( ':mail', $mail, PDO::PARAM_STR);
             //sqlを 実行
             $stmt->execute();
             // メールアドレスをカウント
             $record = $stmt->fetch();
-            if($record['cnt'] > 0) {
+            if ($record['cnt'] > 0) {
                 self::$user_errors['mail'] = "登録済みのメールアドレスです。";
             }
         } else {
@@ -194,8 +202,9 @@ class DB_Controller_users extends DB_Controller {
     }
 
     // ユーザーグループ登録
-    public function insert_user_group($group_name, $group_password) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+    public function insertUserGroup($group_name, $group_password)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('INSERT INTO `user_groups`(`group_name`, `group_password`) VALUES(:group_name, :group_password);');
             //SQL文中の プレース部を 定義しておいた変数に置き換える
             $stmt->bindParam( ':group_name', $group_name, PDO::PARAM_STR);
@@ -209,8 +218,9 @@ class DB_Controller_users extends DB_Controller {
     }
 
     // ユーザーグループ検索
-    public function search_group_id($group_password) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+    public function searchGroupId($group_password)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('SELECT `id`FROM user_groups WHERE group_password=:group_password');
             $stmt->bindParam( ':group_password', $group_password, PDO::PARAM_STR);
             //sqlを 実行
@@ -226,8 +236,9 @@ class DB_Controller_users extends DB_Controller {
      * userテーブル操作用のメソッド
      **********************************************************************/
     // 
-    public function insert_an_user($user_name, $password, $mail, $user_image, $group_id) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+    public function insertUser($user_name, $password, $mail, $user_image, $group_id)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('INSERT INTO users(user_name, password, mail, user_image, group_id) 
                 VALUES(:user_name, :password, :mail, :user_image, :group_id);');
             //SQL文中の プレース部を 定義しておいた変数に置き換える
@@ -244,8 +255,9 @@ class DB_Controller_users extends DB_Controller {
     }
 
     // 論理的削除を行うメソッド
-    public function disable_a_user($target_id) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+    public function disableUser($target_id)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('UPDATE `users` SET `is_deleted`=true WHERE id=:id');
             //SQL文中の プレース部を 定義しておいた変数に置き換える
             $stmt->bindParam( ':id', $target_id, PDO::PARAM_INT);
@@ -255,9 +267,11 @@ class DB_Controller_users extends DB_Controller {
             return self::$connect_error;
         }
     }
+
     // 論理的削除を取り消すメソッド
-    public function delete_a_user($target_id) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+    public function deleteUser($target_id)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('UPDATE `users` SET `is_deleted`=false WHERE id=:id');
             //SQL文中の プレース部を 定義しておいた変数に置き換える
             $stmt->bindParam( ':id', $target_id, PDO::PARAM_INT);
@@ -267,8 +281,10 @@ class DB_Controller_users extends DB_Controller {
             return self::$connect_error;
         }
     }
-    public function fetch_an_image($target_id) {
-        if(isset(self::$pdo) || self::connect_DB()) {
+
+    public function fetchImage($target_id)
+    {
+        if (isset(self::$pdo) || self::connectDB()) {
             $stmt = self::$pdo->prepare('UPDATE `users` SET `is_deleted`=false WHERE id=:id');
             //SQL文中の プレース部を 定義しておいた変数に置き換える
             $stmt->bindParam( ':id', $target_id, PDO::PARAM_INT);
--- users_do.php
+++ users_do.php
@@ -1,18 +1,18 @@
 <?php
-require('class/DB_Controller_users.php');
+require('class/DB_Connector_users.php');
 
 if(!empty($_POST)){
-	$new_user = new DB_Controller_users();
+	$new_user = new DB_Connector_users();
 	// ユーザー登録の際
 	if(isset($_POST['new_user'])) {
-		$user_errors = $new_user->input_confirmation();
+		$user_errors = $new_user->inputConfirmation();
 		if($user_errors == "ok") {
 		// ログインに飛ばす
 		header('Location: ../view/login.php');
 		}
 	// ログインの際
 	} else if(isset($_POST['login_user'])) {
-		$user_errors = $new_user->login_confirmation();
+		$user_errors = $new_user->loginConfirmation();
 		if($user_errors == "login") {
 		// トップページに飛ばす
 		header('Location: ../view/index.php');
