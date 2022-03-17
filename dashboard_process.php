124d21a0e43207b155db6c0a9e8ac92a6117e3d2
AkitoOkamoto <Akito.Okamoto@kusurinomadoguchi.co.jp>
2022-03-17 14:01:28 +0900

    fix: コメント修正

--- dashboard_process.php
+++ dashboard_process.php
@@ -2,7 +2,7 @@
 include('./class/DB_Connector_main.php');
 $DB_connector = new DB_Connector_main();
 
-// 支出とカテゴリを一緒に引っぱってくるメソッド
+// 支出とカテゴリを一緒に引っぱってくるメソッド（追加する）
 $categories = $DB_connector->fetchCategoryColumns();
 
 DB_Connector_main::disconnectDB();
