cd8171ce88961857e561dd8f0382985e8be07fa5
MitoTakuya <Takuya.Mito@kusurinomadoguchi.co.jp>
2022-03-18 15:27:15 +0900

    Add:家計簿パスワード表示、非表示切り替え

--- view/user_show.php
+++ view/user_show.php
@@ -55,7 +55,7 @@ require_once ("../users_do.php");
 					<div class="check"><h5><i class="fas fa-calculator"></i>グループ情報</h5></div>
 				</div>
 				<div class="card-body">
-					<div class="card-text">
+					<div class="card-text" id="groupCard">
 						<?php
 						// ユーザーグループ情報
 						foreach ($user_show as $group) {
@@ -68,9 +68,12 @@ require_once ("../users_do.php");
 						<div class="ml-4">
 							<span><?php echo $group['goal']; ?>円</span>							
 						</div>
-						<div class="check"><h5><i class="fas fa-lock"></i>家計簿パスワード</h5></div>
+						<h5 class="check"><i class="fas fa-lock"></i>家計簿パスワード <span><button class="btn btn-secondary btn-sm" @click="active">表示する</button></span>
+					</h5>
+						
 						<div class="ml-4">
-							<span><?php echo $group['group_password']; ?></span>
+							<span v-if="isActive">*****************</span>
+							<span v-else><?php echo $group['group_password']; ?></span>
 						</div>
 						<?php
 						break;
@@ -81,10 +84,14 @@ require_once ("../users_do.php");
 			</div>
 		</div>
 
+
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<!-- Vue.jsの読み込み -->
+	<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+	<script src="../stylesheet/js/user_show.js"></script>
 	</body>
 </html>
