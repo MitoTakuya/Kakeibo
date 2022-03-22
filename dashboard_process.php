424741ef2fc87b6c9ce97e5a5fcfb1b61d245d8a
AkitoOkamoto <Akito.Okamoto@kusurinomadoguchi.co.jp>
2022-03-22 12:45:56 +0900

    update: グラフ以外一旦完了

--- dashboard_process.php
+++ dashboard_process.php
@@ -1,21 +1,89 @@
 <?php
 include('../class/DB_Connector_main.php');
-$DB_connector = new DB_Connector_main();
+
+if (DB_Connector::connectDB()) {
+    $DB_connector = new DB_Connector_main();
+
+    /********** ユーザー・グループ情報の処理 **********/
+    // 画面上部に表示したりpostしたりする用
+    $kakeibo_name = '家計簿(仮)'; // = $_SESSION['group_name'];
+    $group_id = 1; // = $_SESSION['group_id'];
+    $user_name; // = $_SESSION['user_name']; 
 
 
-if ($_POST['selecting_date']) {
-    $target_date = $_POST['selecting_date'];
+    /********** 表示する期間を決める処理 **********/
+    // "yyyymm"の形でpostされた日付を、"yyyymmdd" (dd = '01')に直して変数に代入する
+    $date_dd = '01';
+    if (isset($_POST['date'])) {
+        $target_date = new DateTime($_POST['date'] . $date_dd);
+    } else {
+        $target_date = new DateTime();
+    }
+    // echo $target_date->format('Ymd');
+
+    // カテゴリごとの支出を取り出す
+    $categorized_outgo_list = $DB_connector->fetchFilteredOutgoList(
+                                group_id: $group_id,
+                                target_date: $target_date->format('Ymd'),
+                            );
+
+    // グラフの上に出力する
+    $displayed_year = $target_date->format('Y');
+    $displayed_month = $target_date->format('n');
+    //print_r($categorized_outgo_list);
+
+
+    /********** 日付のselect-option用のデータを用意する **********/
+    // 登録日を取得する *DBC_users にメソッド追加要相談
+    $registration_date = new DateTime('20190601');
+
+    // 現在日時を取得する
+    $carrent_date = new DateTime();
+
+    // 登録日から最新月までの月のリストを作成する
+    while ($registration_date <= $carrent_date) {
+        $past_dates[] = array(
+                            'year' => $registration_date->format('Y'),
+                            'month' => $registration_date->format('n'),
+                            'year_month' => $registration_date->format('Ym')
+        );
+        $registration_date->modify('+1 months');
+    }
+    // 降順に変更する
+    $past_dates = array_reverse($past_dates);
+
+    // scriptタグに埋め込むために下記のようなjsonに変換する
+    /* e.g.
+    *   "[{ year : '2021', month : 12, year_month : 20211201},
+    *     { year : '2022', month : 1,  year_month : 20220101},... ]"
+    */
+    $jsonized_past_dates = json_encode($past_dates);
+
+
+    /********** 円グラフ用のデータを用意する **********/
+    // var_dump($categorized_outgo_list);
+    foreach ($categorized_outgo_list as $row) {
+        $to_json["labels"][] = $row['category_name'];
+        $to_json["datasets"]["data"][] = $row['payment'];
+    }
+    $to_json["datasets"]["backgroundColor"] = [
+        "#ff0000",  //赤
+        "#E7556E",  //ピンク
+        "#ffaa55",  //薄茶
+        "#ffff80",  //黄色
+        "#aae272",  //緑
+        "#7414A0",
+        "#35C5C7",
+    ];
+
+    // echo "<br>-----<br>";
+    $jsonized_outgo_list =  json_encode($to_json, JSON_UNESCAPED_UNICODE);
+    var_dump($jsonized_outgo_list);
+    DB_Connector::disconnectDB();
+} else {
+    // include('error.php');
+    echo "error view should be displayed";
+    die();
 }
 
-// 支出とカテゴリを一緒に引っぱってくるメソッド（追加する）
-$categories = $DB_connector->fetchCategoryColumns();
-$categorized_outgo_list = array( //DBCの後でメソッドと入れ替える。(groupid: , target_month: , )
-                array('category_id' => 1, 'category_name' => '食費', 'outgo' => 50000),
-                array('category_id' => 2, 'category_name' => '交通費', 'outgo' => 30000),
-                array('category_id' => 3, 'category_name' => '娯楽費', 'outgo' => 10000)
-            );
-$archives = "{ year : '2021', month : 12, year_month : 20211201},
-            { year : '2022', month : 1,  year_month : 20220101},
-            { year : '2022', month : 2,  year_month : 20220201},
-            { year : '2022', month : 3,  year_month : 20220301}";
-DB_Connector_main::disconnectDB();
+
--- view/dashboard.php
+++ view/dashboard.php
@@ -22,7 +22,7 @@ include('../dashboard_process.php');
 
         <header>
             <nav class="navbar navbar-dark bg-dark">
-                <a href="#" class="navbar-brand">家計簿(グループ名)phpで実装</a> 
+                <a href="#" class="navbar-brand"><?= $kakeibo_name ?></a> 
                 <div class="btn-group" role="group">
                     <button id="header_menu" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Menu
@@ -41,50 +41,51 @@ include('../dashboard_process.php');
                 <div class="card card_property">
                     <svg class="card-img-top" width="320" height="75" xmlns="http://www.w3.org/2000/svg" focusable="false" role="img" >
                         <rect width="100%" height="100%" fill="#87CEEB"/>
-                        <text class="mx-auto h3" x="50%" y="50%" fill="#ffffff" dy=".5em" text-anchor="middle">(phpでfetchしたデータ)月の支出</text>
+                        <text class="mx-auto h3" x="50%" y="50%" fill="#ffffff" dy=".5em" text-anchor="middle"><?= $displayed_year ?>年 <?= $displayed_month ?>月 の支出</text>
                     </svg> <!-- フォントを後で変えたい -->
                         <div class="style={direction: rtl;}"> <!-- 可能なら右に寄せたい -->
 
                         <!-- 日付選択 -->
                         <form action="#" method="post" id="selecting_date" :ref="selecting_date">
-                            <select class="w-25 form-control select_period" name="year_month" v-model="selected_date" @input="selfPostDate">
+                            <select class="w-25 form-control select_period" name="date" v-model="selected_date" @input="selfPostDate">
                                 <option disabled value="">--月を選択--</option> <!-- 最新月をデフォルト値にする -->
-                                <!-- vue.jsで実装する -->
-                                <option value="dog">登録月から</option>
-                                <option v-for="date in sample_dates" :value="date.year_month">{{date.year}}年{{date.month}}月</option>
+                                <option v-for="date in selectable_dates" :value="date.year_month">{{date.year}}年{{date.month}}月</option>
                             </select>
                         </form>
                         </div>
-                    <div class="outgo_chart card-body mx-auto">
-                            <img src="https://www.illustkit.com/wp/wp-content/uploads/2021/05/IK-2021-0228-69.png" width="500" height="500">
-                    </div>
-                    <p>(これは仮置きのグラフ)</p>
+                        <!-- 支出割合グラフ -->
+                        <div class="outgo_chart card-body mx-auto" style="width:600px">
+                                <canvas id="outgo_rate" style="width:600px"></canvas>
+                        </div>
+                        
                     </div>
             </div>
 
             <div class="mx-auto">
                 <div class="card card_property">
-                    <p>ここにカテゴリーごとにまとめた支出のテーブルを作る</p>
                         <div class="outgo_chart card-body mx-auto">
-                                <img src="https://shop.woodworks-marutoku.com/kanri/wp-content/uploads/2019/09/190926_ta_top.jpg" width="720" height="400">
-                                <p>(これは仮置きの画像)</p>
-                                <p>(下記のような表を作成するテーブル)</p>
                                 <table class="table">
-                                    <!-- 後で colの属性を指定する -->
-                                    <thead>
-                                        <th>カテゴリー</th>
-                                        <th>支出額</th>
-                                        <!-- <th>前月比とか？</th> -->
-                                    </thead>
-                                    <?php foreach($categorized_outgo_list as $outgo): ?>
-                                    <tr>
-                                        <td> <!-- 別タブで開くように設定予定 getメソッドで送る-->
-                                            <a href="" target="_blank" rel="noopener noreferrer"><?= $outgo['category_name'] ?></a>
-                                        </td>
-                                        <td><?= $outgo['outgo'] ?></td>
-                                        <!-- <td>なにかしらのデータ</td> -->
-                                    </tr>
-                                    <?php endforeach; ?>
+                                    <?php if (count($categorized_outgo_list) === 0):?>
+                                        <!-- もしレコードが存在しなければ「記録がありません」 -->
+                                            <p>記録がありません</p>
+                                    <?php else:?> 
+                                        <!-- 後で colの属性を指定する -->
+                                        <col width="500">
+                                        <thead>
+                                            <th>カテゴリー</th>
+                                            <th>支出額</th>
+                                            <!-- <th>前月比とか？</th> -->
+                                        </thead>
+                                            <?php foreach($categorized_outgo_list as $outgo): ?>
+                                            <tr>
+                                                <td> <!-- 別タブで開くように設定予定 getメソッドで送る-->
+                                                    <a href="" target="_blank" rel="noopener noreferrer"><?= $outgo['category_name'] ?></a>
+                                                </td>
+                                                <td><?= $outgo['payment'] ?></td>
+                                                <!-- <td>なにかしらのデータ</td> -->
+                                            </tr>
+                                            <?php endforeach; ?>
+                                    <?php endif;?>
                                 </table>
                         </div>
                 </div>
@@ -94,7 +95,7 @@ include('../dashboard_process.php');
 
         <footer class="footer text-light bg-dark">
             <nav class="navbar navbar-dark bg-dark">
-                <a href="#" class="navbar-brand ">家計簿(グループ名) *後で位置を修正</a> 
+                <a href="#" class="navbar-brand "><?= $kakeibo_name ?></a> 
             </nav>
         </footer>
         <!-- Optional JavaScript -->
@@ -102,43 +103,69 @@ include('../dashboard_process.php');
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <!-- Vue.js下書き -->
+        <!-- Vue.js -->
         <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+        <!-- Vue-Chart.js -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+        <script src="https://unpkg.com/vue-chartjs/dist/vue-chartjs.min.js"></script>
+        <!-- axios -->
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script>
-            var selecting_date = document.getElementById("selecting_date");
+            // vuejsでの実装方法を確認中のため、仮置き
+            var ctx = document.getElementById('outgo_rate').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                        labels: ['Red', 'Green', 'Blue'],
+                        datasets: [{
+                            data: [10, 20, 30],
+                            backgroundColor: ['#f88', '#484', '#48f'],
+                            weight: 100,
+                        }],
+                    },
+            });
+            console.log(ctx);
+
             let app = new Vue({
             el :'#dashboard',
             data : {
                 selected_date : '', // phpから直に最新月の直接を代入する
-                detail_link : 'outgo_detail.php?ctg=',
-                outgo_table_columns : ["カテゴリー",  "支出額", "(前月比とか？)"],
-                sample_dates : [<?= $archives ?>], //php から読み込む
-                selecting_date : "selecting_date"
+
+                selectable_dates : <?= $jsonized_past_dates ?>, //php から読み込む
+                selecting_date : "selecting_date",
+
+                chart_area : null,
+                outgo_list : <?= $jsonized_outgo_list ?>
             },
             methods : {
-                add_video : function(event) {
-                    //該当phpファイルに動画のurlを送り 動画IDを抽出させるメソッド。
-                    let target_video_url = this.target_video_url;
-                    let params = new URLSearchParams();
-                    params.append('target_video_url', this.target_video_url);
-                    axios
-                    .post('Data/analyse_video_url.php', params)
-                    .then(response => (
-                        this.video_id = response.data
-                    ));
-                    this.add_vid_btn = "btn btn-success";
-                    //console.log("-----end of function shorten_url-----------------------------");
-                },
+                // displayChart : async function(event) {
+                //     // 仮置きしている非同期処理用メソッド
+                //     let params = new URLSearchParams();
+                //     let path = '../ajax_dashbord.php'
+                //     // params.append('param_name', );
+                //     axios
+                //     .post(path, params)
+                //     .then(response => {
+                //         console.log(response.data);
+                //         return response.data
+                //     });
+                // },
                 
                 selfPostDate : function (event) {
                     //$ref['selecting_date'].submit();
                     console.log(this.$refs['selecting_date']);
                     this.$refs['selecting_date'].submit();
                 }
-            }, mounted : function(){
-                // 読み込み時の処理を書く
-                // 
-            }
+            }// ,   後で修正
+            // mounted: function(){
+            //     console.log(this.outgo_list);
+            //     this.chart_area = this.$refs['outgo_chart'];
+            //     console.log(this.$refs);
+            //     // this.outgo_chart = new Chart(this.chart_area, {
+            //     //     type: 'pie',
+            //     //     data: 
+            //     // });
+            // }
         })
         </script>
     </body>
