f8acfaeb208c2934cbc5abf9d1dc279b6fb3d47d
yamauchi1105454 <akio.yamauchi@kusurinomadoguchi.co.jp>
2022-03-28 11:06:57 +0900

    session削除の処理を消した

--- class/Config.php
+++ class/Config.php
@@ -35,20 +35,4 @@ class Config {
         }
     }
 
-    //セッション削除
-    public static function destroy_session() {
-        // セッション開始
-        session_start();  
-        // セッション変数を全て削除
-        $_SESSION = array();
-        // セッションクッキーを削除
-        if (isset($_COOKIE["PHPSESSID"])) {
-        setcookie("PHPSESSID", '', time() - 1800, '/');
-        }
-        // セッションの登録データを削除
-        session_destroy();
-        header('Location: http://localhost/');
-        exit;
-    }
-
 } 
