16aa7d63dc4afb2e2ce19c7b54738e4ca548460a
yamauchi1105454 <akio.yamauchi@kusurinomadoguchi.co.jp>
2022-03-31 12:24:32 +0900

    ヘッダー差し替えました！

--- view/registory.php
+++ view/registory.php
@@ -15,12 +15,9 @@ require_once __DIR__.'/../registoryController.php';
 	<title>記帳画面</title>
 </head>
 <body>
-	<nav class="navbar navbar-dark bg-dark">
-		<a href="login.php" class="navbar-brand">ログイン</a>
-		<a href="users_new.php" class="navbar-brand text-right">新規登録</a>
-	</nav>
+	<!-- ヘッダー -->
+	<?php include __DIR__ . "/_header.php" ?>
 	<div class="container mt-4">
-		<!-- <div class="mt-4"></div> -->
 		<div class="mx-auto">
 			<ul class="tab-group">
 				<li class="tab tab-A is-active">支出</li>
@@ -241,9 +238,15 @@ require_once __DIR__.'/../registoryController.php';
 		<button class="btn btn-danger" id="cancel" type="button">キャンセル</button>
 	</div>
 	
-
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+		integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+		integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<script src="../stylesheet/js/registory.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 </body>
 </html>
