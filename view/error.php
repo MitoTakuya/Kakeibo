7cbbfe26b2a77d70b87fae19b40a0b16f8849f3f
MitoTakuya <Takuya.Mito@kusurinomadoguchi.co.jp>
2022-04-08 16:33:41 +0900

    Fix:エラー画面修正

--- view/error.php
+++ view/error.php
@@ -1,5 +1,15 @@
 <?php
-require_once __DIR__.'/../init.php';
+$uri = basename($_SERVER['REQUEST_URI']);
+$unlogined = false;
+$from_user = false;
+if ($uri == 'userNew.php') {
+    $unlogined = true;
+} else {
+    require_once __DIR__.'/../init.php';
+}
+if ($uri == 'userNew.php'||$uri == 'groupUpdate.php' || $uri == 'userUpdate.php') {
+    $from_user = true;
+}
 ?>
 
 <!doctype html>
@@ -37,20 +47,36 @@ require_once __DIR__.'/../init.php';
 					<span>内容：</span>
 					<?php if (isset($error_message)): ?>
 					<span class="text-danger"><?= $error_message ?></span>
+					<?php else :?>
+					<span class="text-danger">予期せぬエラーが発生しました。</span>
+					<?php endif ;?>
 				</div>
+
+				<?php if (isset($error_message) && $error_message !== "不正な通信です。" && $from_user === false): ?>
 				<input type="button" class="btn btn-block btn-lg btn-primary" value="再読込み"
 					onclick="window.location.reload(true);" />
+				<?php endif ;?>
+
+				<?php if ($unlogined): ?>
+				<input type="button" class="btn btn-block btn-lg btn-primary" value="戻る" onclick="history.back()" />
+				<?php else :?>
 				<a href="http://<?= $_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) ?>/logout.php?error=1"
 					class="nav-item nav-link text-center">再ログイン</a>
-				<?php else :?>
-				<span class="text-danger">予期せぬエラーが発生しました。</span>
+				<?php endif ;?>
 			</div>
-			<a href="http://<?= $_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) ?>/logout.php"
-				class="nav-item nav-link text-center">ログアウト</a>
-			<?php endif ;?>
 		</div>
 	</div>
-	</div>
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+		integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+	</script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+		integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+	</script>
 
 </body>
 
