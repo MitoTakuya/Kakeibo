b1ded40cf08c517e5b402b65de9b88117a482e5a
MitoTakuya <Takuya.Mito@kusurinomadoguchi.co.jp>
2022-04-08 16:35:31 +0900

    Fix:cssファイルのパスを修正

--- view/error.php
+++ view/error.php
@@ -21,8 +21,7 @@ if ($uri == 'userNew.php'||$uri == 'groupUpdate.php' || $uri == 'userUpdate.php'
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
 		integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-	<link rel="stylesheet"
-		href="http://<?= $_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) ?>/stylesheet/css/user.css">
+	<link rel="stylesheet" href="../stylesheet/css/user.css">
 	<title>error</title>
 </head>
 
